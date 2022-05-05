104b9e3ca185aaa417d8809d94003b00dd020e7f
ijTuhin <isratishu0209@gmail.com>
2022-05-06 02:39:33 +0600

    404 page created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './Pages/Authentication/Login/Login';
 import Home from './Pages/HomePage/Home';
 import Blogs from './Pages/Others/Blogs/Blogs';
 import Inventory from './Pages/Others/Inventory/Inventory';
+import PageNotFound from './Pages/Others/PageNotFound/PageNotFound';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/login' element={<Login></Login>}/>
         <Route path='/blogs' element={<Blogs></Blogs>}/>
         <Route path='/inventory' element={<Inventory></Inventory>}/>
+        <Route path='*' element={<PageNotFound></PageNotFound>}/>
       </Routes>
     </div>
   );
---
+++ src/Pages/Others/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <h2 className='text-3xl text-center mt-20'>404 Page not found</h2>
+        </div>
+    );
+};
+
+export default PageNotFound;
