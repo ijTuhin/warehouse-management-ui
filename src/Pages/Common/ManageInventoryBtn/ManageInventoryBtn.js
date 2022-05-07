3d3a6538a919158daa39d759e8c82b01b5018977
ijTuhin <isratishu0209@gmail.com>
2022-05-07 11:35:44 +0600

    firebase installed & initialized

--- src/Pages/Common/ManageInventoryBtn/ManageInventoryBtn.js
+++ src/Pages/Common/ManageInventoryBtn/ManageInventoryBtn.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ManageInventoryBtn = () => {
     return (
         <div className='flex justify-center my-10'>
-            <h2 className='border border-gray-600 rounded px-2.5 py-0.5'>Manage Inventories</h2>
+            <button type='submit' className='border border-gray-600 rounded px-2.5 py-0.5'>Manage Inventories</button>
         </div>
     );
 };
---
+++ src/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCe8iklv2irSwk039HdhURd3aJqx1FDLLM",
+  authDomain: "warehouse-management-ui.firebaseapp.com",
+  projectId: "warehouse-management-ui",
+  storageBucket: "warehouse-management-ui.appspot.com",
+  messagingSenderId: "802747567687",
+  appId: "1:802747567687:web:8cc0fab0e19db89cc66b62"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
