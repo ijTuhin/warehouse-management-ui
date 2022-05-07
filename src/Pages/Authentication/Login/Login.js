4eaacb15053b12028f08ca840969641df973f9ea
ijTuhin <isratishu0209@gmail.com>
2022-05-08 02:47:29 +0600

    reset password option implemented

--- src/Pages/Authentication/Login/Login.js
+++ src/Pages/Authentication/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Google } from '../../../index';
-import { useCreateUserWithEmailAndPassword, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Loading from '../Loading/Loading';
@@ -12,6 +12,7 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
     const [notRegistered, setNotRegistered] = useState(false);
+    const [reset, setReset] = useState(false);
     const location = useLocation();
     const navigate = useNavigate();
     let error;
@@ -28,7 +29,9 @@ const Login = () => {
         createUserWithEmailAndPassword,
         userCreated,
         loading2,
-    ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+    const [sendPasswordResetEmail] = useSendPasswordResetEmail(auth);
 
     const handleUserCheck = event => {
         setNotRegistered(event.target.checked);
@@ -42,6 +45,37 @@ const Login = () => {
         }
         else {
             signInWithEmailAndPassword(email, password);
+        }
+    }
+    const resetPassword = async () => {
+        await sendPasswordResetEmail(email);
+        await toast('Check your email');
+        await setReset(true);
+    }
+
+    if (reset) {
+        error =
+            <>
+                <div class="bg-green-100 rounded-lg py-5 px-6 mb-3 text-base text-green-700 inline-flex items-center w-full" role="alert">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path>
+                    </svg>
+                    Check email to update password
+                </div>
+            </>
+    }
+
+    const loginBtnClicked = () => {
+        if (loginError) {
+            error =
+                <>
+                    <div className="bg-red-100 rounded py-3.5 px-6 mb-3 text-base text-red-700 inline-flex items-center w-full" role="alert">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times-circle" className="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path>
+                        </svg>
+                        {loginError?.message}
+                    </div>
+                </>
         }
     }
 
@@ -60,19 +94,6 @@ const Login = () => {
     })
 
 
-
-    if (loginError) {
-        console.error(loginError);
-        error =
-            <>
-                <div className="bg-red-100 rounded py-3.5 px-6 mb-3 text-base text-red-700 inline-flex items-center w-full" role="alert">
-                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times-circle" className="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path>
-                    </svg>
-                    {loginError?.message}
-                </div>
-            </>
-    }
 
     return (
         <div className="container h-screen flex flex-col justify-start items-center px-6 py-12">
@@ -123,12 +144,12 @@ const Login = () => {
                         <label className="form-check-label inline-block text-gray-800" htmlFor="exampleCheck2"
                         >Not registered?</label>
                     </div>
-                    <a
-                        href="#!"
-                        className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
-                    >Forgot password?</a>
+                    <button
+                        type='submit'
+                        className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out" onClick={resetPassword}
+                    >Forgot password?</button>
                 </div>
-                <button
+                <button onClick={loginBtnClicked}
                     type="submit"
                     className="px-7 py-2 md:py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg w-full"
                 >
@@ -138,10 +159,10 @@ const Login = () => {
                     loading ?
                         <><Loading></Loading></>
                         :
-                    loading2 ?
-                        <><Loading></Loading></>
-                        :
-                        <></>
+                        loading2 ?
+                            <><Loading></Loading></>
+                            :
+                            <></>
                 }
                 <div
                     className="flex items-center my-2.5 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
