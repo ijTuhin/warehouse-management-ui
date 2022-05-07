10322cda8cd5c129570f17f5c1338406e7a6ebad
ijTuhin <isratishu0209@gmail.com>
2022-05-08 03:15:32 +0600

    login with pass-email done completely

--- src/Pages/Authentication/Login/Login.js
+++ src/Pages/Authentication/Login/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
     const [notRegistered, setNotRegistered] = useState(false);
-    const [reset, setReset] = useState(true);
+    const [notReset, setNotReset] = useState(false);
     const location = useLocation();
     const navigate = useNavigate();
     let error;
@@ -49,29 +49,13 @@ const Login = () => {
     }
 
     const handleLoginBtnClicked = () => {
-        setReset(true)
+        setNotReset(true)
     }
-
+    
 
     const resetPassword = async () => {
         await sendPasswordResetEmail(email);
         await toast('Email sent to reset password')
-    }
-
-
-    if (!reset) {
-        error = <></>
-    }
-    else {
-        error =
-            <>
-                <div className="bg-red-100 rounded py-3.5 px-6 mb-3 text-base text-red-700 inline-flex items-center w-full" role="alert">
-                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times-circle" className="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path>
-                    </svg>
-                    {loginError?.message}
-                </div>
-            </>
     }
 
 
@@ -88,6 +72,21 @@ const Login = () => {
             navigate(from, { replace: true });
         }
     })
+
+    if (notReset) {
+        error =
+            <>
+                <div className="bg-red-100 rounded py-3.5 px-6 mb-3 text-base text-red-700 inline-flex items-center w-full" role="alert">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times-circle" className="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path>
+                    </svg>
+                    {loginError?.message}
+                </div>
+            </>
+    }
+    else{
+        error = <></>
+    }
 
 
 
