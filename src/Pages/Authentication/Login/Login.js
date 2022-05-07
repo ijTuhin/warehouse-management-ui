42c20477a15b758301393b17e3119a71a6093d0f
ijTuhin <isratishu0209@gmail.com>
2022-05-07 14:45:06 +0600

    error message for login attempts

--- src/Pages/Authentication/Login/Login.js
+++ src/Pages/Authentication/Login/Login.js
@@ -9,13 +9,14 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const location = useLocation();
     const navigate = useNavigate();
-
+    let error;
     const from = location.state?.from?.pathname || "/";
 
     const [
         signInWithEmailAndPassword,
         user,
         loading,
+        loginError
     ] = useSignInWithEmailAndPassword(auth);
 
     const handleFormSubmit = event => {
@@ -30,10 +31,23 @@ const Login = () => {
         navigate(from, { replace: true });
     }
 
+    if (loginError) {
+        error =
+            <>
+                <div class="bg-red-100 rounded py-3.5 px-6 mb-3 text-base text-red-700 inline-flex items-center w-full" role="alert">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path>
+                    </svg>
+                    {loginError?.message}
+                </div>
+            </>
+    }
+
     return (
         <div class="container h-screen flex flex-col justify-start items-center px-6 py-12">
             <form className='w-72 md:w-96' onSubmit={handleFormSubmit}>
                 <h2 className='md:text-3xl text-2xl w-full mb-5'>Please Login to Continue</h2>
+                {error}
                 <div class="mb-3">
                     <input
                         type="text"
@@ -64,9 +78,9 @@ const Login = () => {
                 </button>
                 {
                     loading ?
-                    <><Loading></Loading></>
-                    :
-                    <></>
+                        <><Loading></Loading></>
+                        :
+                        <></>
                 }
                 <div
                     class="flex items-center my-2.5 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -4,9 +4,6 @@ import Footer from '../Common/Footer/Footer';
 import Banner from './Banner/Banner';
 import Items from './Items/Items';
 import ManageInventoryBtn from '../Common/ManageInventoryBtn/ManageInventoryBtn';
-import Loading from '../Authentication/Loading/Loading';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
 
 const Home = () => {
     return (
