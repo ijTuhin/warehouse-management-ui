18a6e28065dd0ea8fbe41a0f34f1062c8cb6a0d4
ijTuhin <isratishu0209@gmail.com>
2022-05-09 19:14:31 +0600

    updated dropdown menu list for logged in user

--- src/Pages/Common/Header/Header.js
+++ src/Pages/Common/Header/Header.js
@@ -10,7 +10,7 @@ import { signOut } from 'firebase/auth';
 const Header = () => {
     const [user] = useAuthState(auth);
     const navigate = useNavigate();
-    
+
     return (
         <div className='sticky z-50 top-0 bg-white flex justify-between items-center shadow-md px-5'>
             <div className='my-2.5 font-mono'>
@@ -24,20 +24,20 @@ const Header = () => {
                 <Link className='p-3 bar hover:text-black' to="/about">About</Link>
                 {
                     !user ?
-                    <></>
-                    :
-                    <>
-                    <Link className='p-3 bar hover:text-black' to="/manage-inventories">Manage Item</Link>
-                    <Link className='p-3 bar hover:text-black' to="/add-items">Add Item</Link>
-                    <Link className='p-3 bar hover:text-black' to="/my-items">My Items</Link>
-                    </>
+                        <></>
+                        :
+                        <>
+                            <Link className='p-3 bar hover:text-black' to="/manage-inventories">Manage Item</Link>
+                            <Link className='p-3 bar hover:text-black' to="/add-items">Add Item</Link>
+                            <Link className='p-3 bar hover:text-black' to="/my-items">My Items</Link>
+                        </>
                 }
             </nav>
             {
                 !user ?
                     <><img className='w-10 m-3 hidden md:block' src={blank} alt="" /></>
                     :
-                    <><button className='p-3 hover:text-black' onClick={() => {
+                    <><button className='p-3 hover:text-black md:block hidden' onClick={() => {
                         signOut(auth);
                         navigate('/');
                     }}>LogOut</button></>
@@ -53,7 +53,7 @@ const Header = () => {
                             aria-expanded="false">
                             <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
                         </Link>
-                        <ul className=" dropdown-menu min-w-max absolute bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none"
+                        <ul className=" dropdown-menu min-w-max absolute bg-white text-base z-50 float-left list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none"
                             aria-labelledby="dropdownMenuButton2">
                             <li>
                                 <Link className=" dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 "
@@ -63,6 +63,25 @@ const Header = () => {
                                 <Link className=" dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 "
                                     to="/products">Products</Link>
                             </li>
+                            {
+                                !user ?
+                                    <></>
+                                    :
+                                    <>
+                                        <li>
+                                            <Link className=" dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 "
+                                                to="/manage-inventories">Manage Item</Link>
+                                        </li>
+                                        <li>
+                                            <Link className=" dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 "
+                                                to="/add-items">Add Item</Link>
+                                        </li>
+                                        <li>
+                                            <Link className=" dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 "
+                                                to="/my-items">My Items</Link>
+                                        </li>
+                                    </>
+                            }
                             <li>
                                 <Link className=" dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 "
                                     to="/blogs">Blog</Link>
@@ -71,6 +90,17 @@ const Header = () => {
                                 <Link className=" dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 "
                                     to="/about">About</Link>
                             </li>
+                            {
+                                !user ?
+                                    <></>
+                                    :
+                                    <>
+                                        <button className='p-0.5 hover:text-black bg-slate-200 hover:bg-slate-300 border-t-2 w-full' onClick={() => {
+                                            signOut(auth);
+                                            navigate('/');
+                                        }}>LogOut</button>
+                                    </>
+                            }
                         </ul>
                     </div>
                 </div>
