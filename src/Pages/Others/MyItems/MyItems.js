a6bf21ba34af547647142d42c5a70aea48a716ce
ijTuhin <isratishu0209@gmail.com>
2022-05-09 23:10:27 +0600

    showing user added individual product on ui

--- src/Pages/Others/MyItems/MyItems.js
+++ src/Pages/Others/MyItems/MyItems.js
@@ -1,114 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { img2, img3 } from '../../../index';
 import Footer from '../../Common/Footer/Footer';
 import Header from '../../Common/Header/Header';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import './MyItems.css';
+import PerUserItem from './PerUserItem';
+import auth from '../../../firebase.init';
 const MyItems = () => {
+    const [user] = useAuthState(auth);
+    const [items, setItems] = useState([]);
+    const [userItems, setUserItems] = useState([]);
+    useEffect(() => {
+        fetch('https://quiet-hamlet-97708.herokuapp.com/item')
+            .then(res => res.json())
+            .then(data => {
+                setItems(data)
+                const myItems = items.filter(item => item.email === user.email);
+                setItems(myItems);
+            });
+    }, [])
+
     return (
         <div>
             <Header></Header>
-            <ul className='grid md:grid-cols-4 grid-cols-1 md:m-20 m-5'>
-                <li>
-                    <div className="md:mx-4 md:my-6 relative">
-                        <a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300" href='?#'>
-                            <p className="tracking-wider" >Title</p>
-                            <p className="mx-auto">Description</p>
-                        </a>
-                        <a href="/#" className="relative">
-                            <div className="h-48 flex flex-wrap content-center">
-                                <img src={img2} className="mx-auto w-96" alt="" />
-                            </div>
-                        </a>
-                    </div>
-                </li>
-                <li><div className="md:mx-4 md:my-6 relative">
-                    <a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300" href='?#'>
-                        <p className="tracking-wider" >Title</p>
-                        <p className="mx-auto">Description</p>
-                    </a>
-                    <a href="/#" className="relative">
-                        <div className="h-48 flex flex-wrap content-center">
-                            <img src={img3} className="mx-auto w-96" alt="" />
-                        </div>
-                    </a>
-                </div>
-                </li>
-                <li>
-                    <div className="md:mx-4 md:my-6 relative">
-                        <a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300" href='?#'>
-                            <p className="tracking-wider" >Title</p>
-                            <p className="mx-auto">Description</p>
-                        </a>
-                        <a href="/#" className="relative">
-                            <div className="h-48 flex flex-wrap content-center">
-                                <img src={img2} className="mx-auto w-96" alt="" />
-                            </div>
-                        </a>
-                    </div>
-                </li>
-                <li><div className="md:mx-4 md:my-6 relative">
-                    <a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300" href='?#'>
-                        <p className="tracking-wider" >Title</p>
-                        <p className="mx-auto">Description</p>
-                    </a>
-                    <a href="/#" className="relative">
-                        <div className="h-48 flex flex-wrap content-center">
-                            <img src={img3} className="mx-auto w-96" alt="" />
-                        </div>
-                    </a>
-                </div>
-                </li>
-                <li><div className="md:mx-4 md:my-6 relative">
-                    <a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300" href='?#'>
-                        <p className="tracking-wider" >Title</p>
-                        <p className="mx-auto">Description</p>
-                    </a>
-                    <a href="/#" className="relative">
-                        <div className="h-48 flex flex-wrap content-center">
-                            <img src={img3} className="mx-auto w-96" alt="" />
-                        </div>
-                    </a>
-                </div>
-                </li>
-                <li>
-                    <div className="md:mx-4 md:my-6 relative">
-                        <a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300" href='?#'>
-                            <p className="tracking-wider" >Title</p>
-                            <p className="mx-auto">Description</p>
-                        </a>
-                        <a href="/#" className="relative">
-                            <div className="h-48 flex flex-wrap content-center">
-                                <img src={img2} className="mx-auto w-96" alt="" />
-                            </div>
-                        </a>
-                    </div>
-                </li>
-                <li><div className="md:mx-4 md:my-6 relative">
-                    <a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300" href='?#'>
-                        <p className="tracking-wider" >Title</p>
-                        <p className="mx-auto">Description</p>
-                    </a>
-                    <a href="/#" className="relative">
-                        <div className="h-48 flex flex-wrap content-center">
-                            <img src={img3} className="mx-auto w-96" alt="" />
-                        </div>
-                    </a>
-                </div>
-                </li>
-                <li>
-                    <div className="md:mx-4 md:my-6 relative">
-                        <a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300" href='?#'>
-                            <p className="tracking-wider" >Title</p>
-                            <p className="mx-auto">Description</p>
-                        </a>
-                        <a href="/#" className="relative">
-                            <div className="h-48 flex flex-wrap content-center">
-                                <img src={img2} className="mx-auto w-96" alt="" />
-                            </div>
-                        </a>
-                    </div>
-                </li>
-            </ul>
+            <div className='grid grid-cols-1 md:grid-cols-3 my-10 gap-y-5 mx-auto md:mx-56'>
+                {
+                    items.map(item => <PerUserItem
+                        key={item._id}
+                        item={item}
+                    >
+                    </PerUserItem>)
+                }
+            </div>
             <Footer></Footer>
         </div>
     );
---
+++ src/Pages/Others/MyItems/PerUserItem.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+const PerUserItem = ({ item }) => {
+    const {
+        _id,
+        price,
+        img,
+        name,
+        supplier,
+        quantity,
+        sold,
+        services,
+        email
+     } = item;
+    const navigate = useNavigate();
+    const updateItem = _id => {
+        navigate(`/inventory/${_id}`);
+    }
+    console.log(email)
+
+    return (
+        <div className="flex justify-center cards">
+            <div className="block rounded-lg shadow-lg bg-white text-center m-2 relative">
+                <div className='p-2 w-80 h-80 relative card-head'>
+                    <img className="rounded-t-lg" src={img} alt="" />
+                    <div className="p-2 absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
+                        <h5 className="text-gray-900 text-xl font-medium mb-2">{name}</h5>
+                        <p className="text-gray-700 text-base mb-4">Service: {services}</p>
+                        <p className="text-gray-700 text-base mb-4">Price: {price} </p>
+                        <p className="text-gray-700 text-base mb-4">Quantity: {quantity}</p>
+                        <p className="text-gray-700 text-base mb-4">Supplier: {supplier}</p>
+                        <p className="text-gray-700 text-base mb-4">Sold: {sold}</p>
+                    </div>
+                </div>
+                <div className='w-full absolute bottom-0 z-20'>
+                    <button type="submit" onClick={() => updateItem(_id)} className="w-full py-3 bg-slate-600 text-white font-medium text-xs leading-tight uppercase rounded rounded-t-none shadow-md hover:bg-red-600 hover:shadow-lg focus:bg-slate-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Update Item</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PerUserItem;
