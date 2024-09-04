2394007dd5b6883b08bcc65373887a7f76cdd5a2
Israt Jahan Tuhin <90187656+ijTuhin@users.noreply.github.com>
2024-09-04 19:27:54 +0600

    Update server link

--- src/Pages/Others/Products/Products.js
+++ src/Pages/Others/Products/Products.js
@@ -7,7 +7,7 @@ const Products = () => {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        fetch('https://quiet-hamlet-97708.herokuapp.com/item')
+        fetch('https://warehouse-management-db.onrender.com/item')
             .then(res => res.json())
             .then(data => setItems(data));
     }, [])
